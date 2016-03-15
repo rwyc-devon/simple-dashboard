bca6d5263d3c5257b54ebb7c6de07d8873ea078b
dn3s <d@devonsawatzky.ca>
2016-03-14 23:22:03 -0500

    made it a bit more functional and stuff

--- widgets/example.php
+++ widgets/example.php
@@ -4,7 +4,7 @@
 #These are the bare minimum functions. Change "example" in each function name to the name of your widget. so instead of widget_timeout_example it'd be widget_timemout_coffeemaker or whatever. Similarly, name the php file after your widget (has to be exact). So instead of example.php you'd call it coffeemaker.php or whatever. The name has to be exact.
 
 function widget_timeout_example() {
-	return -1; #How often to refresh in seconds. -1 means never reload. 0 means spam the server like crazy. Don't set it to 0.
+	return 1; #How often to refresh in seconds. -1 means never reload. 0 means spam the server like crazy. Don't set it to 0.
 	#A cool trick is that this is called on every refresh, and the client javascript updates the interval for each refresh.
 	#So you could programmatically set this based on when it next needs to reload.
 	#An example is a "now playing" widget that sets the timeout based on how much is left in the song.
@@ -23,13 +23,14 @@ function widget_data_example() { #return the data you want printed. In this case
 	return $result;
 }
 function widget_status_example() { #return either "dead", "good", "normal", "warn", or "critical". Hopefully it's obvious when to return what. Except maybe "dead". That basically just means "the widget isn't working right", not "[important thing] is dead". Save that for "critical"
-	return "dead";
+	return "normal";
 }
 function widget_html_example() { #return the html that'll go in your widget. NOTE: the wrapper and header are already supplied (now you know what the title_* function is for!)
 	#if you want fancy things like a bar graph or an icon, have an outer element, like a <div> and read on!
 	#for a bar, give the <div> a 'has-bar' class, and create a <span> with a class of 'bargraph', and style the width as a percentage. I mean really just look at the exampe below.
 	#for an icon, set the attribute 'data-icon' to the unicode symbol for the icon you want, or a letter or something I guess. Sometime I'll add an icon font so there's more options than just Unicode's "misc symbols".
+	#there are CSS classes for .money and .percent. They just use ::before and ::after stuff to add a $ or % symbol in a lighter color. Note that it conflicts with the icon thing, so make sure not to give the same element a data-icon attribute and one of these classes.
 	$data=widget_data_example();
-	$percent=35; #IRL you'd probably set this to something relevant
-	return "<div class='has-bar' data-icon='&#9742'><span class='bargraph' style='width:$percent%'></span><span class='money'>$data</span></div>";
+	$percent=date("s")/60*100;
+	return "<div class='has-bar' data-icon='&#9742'><span class='bargraph' style='width:$percent%'></span><span>$data</span></div>";
 }
