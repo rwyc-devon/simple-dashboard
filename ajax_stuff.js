c8e7ecf275078196797001e28619e6b736e983ea
dn3s <d@devonsawatzky.ca>
2016-04-25 10:33:18 -0500

    added auto-retry if widget fails to update

--- ajax_stuff.js
+++ ajax_stuff.js
@@ -1,6 +1,9 @@
 function enqueueWidget(e)
 {
-	var timeout=e.getAttribute("data-timeout");
+	enqueueWidget(e, e.getAttribute("data-timeout") || 5);
+}
+function enqueueWidget(e, timeout)
+{
 	if(timeout>=0) {
 		setTimeout(function(){reloadWidget(e)}, timeout*1000);
 	}
@@ -11,11 +14,14 @@ function reloadWidget(e)
 	var widget=e.getAttribute("data-widget");
 	var section=e.getAttribute("data-section");
 	var xhr=new XMLHttpRequest();
-	xhr.addEventListener("load", function(){
+	xhr.addEventListener("load", function() {
 		var newE=new DOMParser().parseFromString(this.response, "text/html").body.childNodes[0];
 		e.parentNode.replaceChild(newE, e);
 		enqueueWidget(newE);
 	});
+	xhr.addEventListener("error", function() {
+		enqueueWidget(e, 1);
+	}
 	xhr.open("GET", "?widget="+widget+"&section="+section);
 	e.classList.add("pending");
 	xhr.send();
